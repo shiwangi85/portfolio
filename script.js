f2f82eb0ed151149e17e61bce4ebcba861b78423
shiwangi85 <158815101+shiwangi85@users.noreply.github.com>
2024-02-04 16:22:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.post('/send-email', (req, res) => {
+    const { email, message } = req.body;
+
+    // Create a nodemailer transporter using Gmail SMTP
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'shiwangii5734@gmail.com',
+            pass: '8448045188'
+        }
+    });
+
+    // Define email options
+    const mailOptions = {
+        from: email,
+        to: 'recipient@gmail.com',
+        subject: 'New Contact Form Inquiry',
+        text: message
+    };
+
+    // Send the email
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error(error);
+            res.status(500).send('Internal Server Error');
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.status(200).send('Email sent successfully');
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
+
+
+
+
+
+
+
+
+
